9aa3f27034f0999b0ca90c4268b00e593c86e31b
RyanM15 <77285830+RyanM15@users.noreply.github.com>
2021-10-20 18:45:15 -0400

    Updated Printer file took 4 min 30 seconds to type

--- Printer.java
+++ Printer.java
@@ -1,21 +1,21 @@
-//Printer.java
+public class Printer{
+    System.out.print("Thank you for shopping at Ryan's Kiosk");
+    Kiosk(numApples);
+    Kiosk(numSteak);
+    Kiosk(numSoda);
+    Kiosk(numCereal);
+    Kiosk(numBread);
+    Kiosk(appleP);
+    Kiosk(steakP);
+    Kiosk(sodaP);
+    Kiosk(cerealP);
+    Kiosk(breadP);
 
-/* This is a java file 'stub' or piece of a file designed to get you started on 
-* learning to code in java, specifically with multiple classes and methods that rely on the other.
-*
-*For this assignment, I would like you to fork, modify and commit this project, while
-* adding the following elements to your code.
-*
-* The receipt printer file could take arguments or parametrs from another class
-* This file will be purely for output
-****************************************
-* 40 characters is a standard width    * 
-* for typical receipt paper, your task *
-* is to make output from the screen    *
-* look like it is printing to a small  *
-* receipt type printer, with all the   *
-* text formatted to 40 characters wide *
-*                                      *
-* Have fun with the assignment!!       *
-****************************************
-*/
+    if (numApples > 0)
+    {
+        System.out.print(numApples + "\t apple \t" + appleP); //need to fix so space even
+
+    }
+    else
+
+
