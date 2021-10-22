89db3c2a2e74e3c573f2d9b8e39583ce363a2385
RyanM15 <77285830+RyanM15@users.noreply.github.com>
2021-10-22 09:26:15 -0400

    worked on in class

--- Printer.java
+++ Printer.java
@@ -1,21 +1,90 @@
 public class Printer{
-    System.out.print("Thank you for shopping at Ryan's Kiosk");
-    Kiosk(numApples);
-    Kiosk(numSteak);
-    Kiosk(numSoda);
-    Kiosk(numCereal);
-    Kiosk(numBread);
-    Kiosk(appleP);
-    Kiosk(steakP);
-    Kiosk(sodaP);
-    Kiosk(cerealP);
-    Kiosk(breadP);
+    //Kiosk kiosk = new Kiosk();
+    
+    int numApples = 0;
+	int numSteak = 0;
+	int numSoda = 0;
+	int numCereal = 0;
+	int numBread = 0;
 
+    
+    
+    
+    
+    
+    public Printer(int numA, int numSt, int numSo, int numC, int numB){
+		numApples = numA;
+		numSteak = numSt;
+		numSoda = numSo;
+        numCereal = numC;	
+        numBread = numB;
+	}
+    
+   
+   
+   
+   
+   
+   
+    int appleP = 1;
+	int steakP = 9;
+	int sodaP = 2;
+	int cerealP = 3;
+	int breadP = 5;
+    
+    
+    public void Checkout(){
+		
+    System.out.print("\nThank you for shopping at Ryan's Kiosk!");
     if (numApples > 0)
     {
-        System.out.print(numApples + "\t apple \t" + appleP); //need to fix so space even
+        System.out.println("\n" + numApples + "\t apple      \t" + "$" +appleP); //need to fix so space even
 
     }
     else
+    {
+        System.out.print("");
+    }
+    if (numSteak > 0)
+    {
+        System.out.println(numSteak + "\t steaks \t" + "$" +steakP); //need to fix so space even
+
+    }
+    else
+    {
+        System.out.print("");
+    }
+    if (numSoda > 0)
+    {
+        System.out.println(numSoda + "\t soda      \t" + "$" +sodaP); //need to fix so space even
+
+    }
+    else
+    {
+        System.out.print("");
+    }
+    if (numCereal > 0)
+    {
+        System.out.println(numCereal + "\t cereal \t" + "$" +cerealP); //need to fix so space even
+
+    }
+    else
+    {
+        System.out.print("");
+    }
+    if (numBread > 0)
+    {
+        System.out.println(numBread + "\t bread      \t" + "$" +breadP); //need to fix so space even
+
+    }
+    else
+    {
+        System.out.print("");
+    }
+    
+}
+}
+
+
 
 
